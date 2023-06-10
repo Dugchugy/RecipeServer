34adc991d781edd2445501c44a72b5e214b2d39e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2023-06-10 16:36:53 -0700

    added main function to run site

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "DugsServer.hpp"
+#include <iostream>
+#include <unistd.h>
+
+int main(){
+    //creates an instance of the recipe server object
+    RecipeServer::RecipeServer recServer("127.0.0.1", "8080");
+
+    recServer.start();
+
+    std::cout << "server started\n";
+
+    int stopInt = -1;
+
+    while(true){
+
+        sleep(1);
+
+        std::cout << "enter (1) to stop the server: ";
+        std::cin >> stopInt;
+
+        if(stopInt == 1){
+            break;
+        }
+
+    }
+
+    std::cout << "server stopped\n";
+
+    recServer.stop();
+    
+    return 0
+}
