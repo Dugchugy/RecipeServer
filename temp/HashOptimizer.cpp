8c8d8899060d3fec8cb5e8a179e0cf12c30ff80b
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2023-11-29 10:43:33 -0800

    Update HashOptimizer.cpp

--- temp/HashOptimizer.cpp
+++ temp/HashOptimizer.cpp
@@ -86,6 +86,11 @@ int main(){
                 //sets results to an array of 103 0s
                 results = new int[103];
 
+                //sets results to 0
+                for(int i =0; i < 103; i++){
+                    results[i] = 0;
+                }
+
                 for(int i = 0; i < 100; i++){
                     //finds the key for the current input
                     unsigned int key = TrunkHash(input[i], curDigits);
