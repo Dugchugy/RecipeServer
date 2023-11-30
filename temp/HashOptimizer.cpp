1d9bcb10d22594d06b079a1106a9b87ea32c5136
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2023-11-29 16:25:19 -0800

    scaled up to consider all 16 digits

--- temp/HashOptimizer.cpp
+++ temp/HashOptimizer.cpp
@@ -93,38 +93,62 @@ int main(){
     for(curDigits[0] = 0; curDigits[0] < 17; curDigits[0] ++){
         for(curDigits[1] = 0; curDigits[1] < 17; curDigits[1] ++){
             for(curDigits[2] = 0; curDigits[2] < 17; curDigits[2] ++){
+                for(curDigits[4] = 0; curDigits[4] < 17; curDigits[4] ++){
+                    for(curDigits[5] = 0; curDigits[5] < 17; curDigits[5] ++){
+                        for(curDigits[6] = 0; curDigits[6] < 17; curDigits[6] ++){
+                            for(curDigits[7] = 0; curDigits[7] < 17; curDigits[7] ++){
+                                for(curDigits[8] = 0; curDigits[8] < 17; curDigits[8] ++){
+                                    for(curDigits[9] = 0; curDigits[9] < 17; curDigits[9] ++){
+                                        for(curDigits[10] = 0; curDigits[10] < 17; curDigits[10] ++){
+                                            for(curDigits[11] = 0; curDigits[11] < 17; curDigits[11] ++){
+                                                for(curDigits[12] = 0; curDigits[12] < 17; curDigits[12] ++){
+                                                    for(curDigits[13] = 0; curDigits[13] < 17; curDigits[13] ++){
+                                                        for(curDigits[14] = 0; curDigits[14] < 17; curDigits[14] ++){
+                                                            for(curDigits[15] = 0; curDigits[15] < 17; curDigits[15] ++){
 
-                //sets results to an array of 103 0s
-                results = new int[103];
+                                                                //sets results to an array of 103 0s
+                                                                results = new int[103];
 
-                //sets results to 0
-                for(int i =0; i < 103; i++){
-                    results[i] = 0;
-                }
+                                                                //sets results to 0
+                                                                for(int i =0; i < 103; i++){
+                                                                    results[i] = 0;
+                                                                }
 
-                for(int i = 0; i < 100; i++){
-                    //finds the key for the current input
-                    unsigned int key = TrunkHash(input[i], curDigits);
-                    //adds it to the results
-                    results[key] += 1;
-                }
+                                                                for(int i = 0; i < 100; i++){
+                                                                    //finds the key for the current input
+                                                                    unsigned int key = TrunkHash(input[i], curDigits);
+                                                                    //adds it to the results
+                                                                    results[key] += 1;
+                                                                }
 
-                //ranks the result
-                float avg = rankOutput(results);
+                                                                //ranks the result
+                                                                float avg = rankOutput(results);
 
-                //std::cout << "found average: " << avg << std::endl;
+                                                                //std::cout << "found average: " << avg << std::endl;
 
-                if(avg < bestAvg){
-                    //saves the current best values
-                    bestAvg = avg;
-                    for(int i = 0; i < 16; i++){
-                        bestDigs[i] = curDigits[i];
+                                                                if(avg < bestAvg){
+                                                                    //saves the current best values
+                                                                    bestAvg = avg;
+                                                                    for(int i = 0; i < 16; i++){
+                                                                        bestDigs[i] = curDigits[i];
+                                                                    }
+                                                                }
+
+                                                                delete[] results;
+
+                                                                results = nullptr;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
-
-                delete[] results;
-
-                results = nullptr;
 
 
 
